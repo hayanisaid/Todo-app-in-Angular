a97ced31a9ad5f08de5b35953b1030332b43452c
hayani said <hayanisaid1995@gmail.com>
2018-03-04 18:03:10 +0000

    add styles,font-google,material-icons,submit event,delete event,animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component,trigger,animate,style,transition,keyframes } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+   trigger("moveInLeft",[
+      transition("void=> *",[style({transform:"translateX(300px)"}),
+        animate(200,keyframes([
+         style({transform:"translateX(300px)"}),
+         style({transform:"translateX(0)"})
+ 
+          ]))]),
+
+
+          transition("*=>void",[style({transform:"translateX(0px)"}),
+        animate(100,keyframes([
+         style({transform:"translateX(0px)"}),
+         style({transform:"translateX(300px)"})
+ 
+          ]))])    
+     
+   	])
+
+  ]
 })
 export class AppComponent {
-  todos=[]
+  todoArray=[];
+  todo;
 
 
    addTodo(value){
-    this.todos.push(value)
-    console.log(this.todos)
+    this.todoArray.push(value)
+    //console.log(this.todos)
+  }
+
+  /*delete item*/
+  deleteItem(todo){
+  	for(let i=0 ;i<= this.todoArray.length ;i++){
+  		if(todo== this.todoArray[i]){
+  			this.todoArray.splice(i,1)
+  		}
+  	}
+  }
+
+  // submit Form
+  todoSubmit(value:any){
+    this.todoArray.push(value.todo)
+     value.todo=""
   }
  
 }
